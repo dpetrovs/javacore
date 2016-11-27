c8e611aa160614e45e5709887cdc3576826a5c33
dpetrovs <dspetrovskyy@gmail.com>
2016-11-28 00:19:28 +0200

    lesson 1

---
+++ src/main/java/com/dpetrovs/app/MathFunc.java
@@ -0,0 +1,11 @@
+package com.dpetrovs.app;
+
+/**
+ * Created by Dmitrik on 28.11.2016.
+ */
+public class MathFunc {
+    public static int multiply(int a, int b) {
+
+        return a * b;
+    }
+}
