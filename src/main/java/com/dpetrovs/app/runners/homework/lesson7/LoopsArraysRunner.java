cd5aaccbe3315495a5ad33b2b21ca22362b6c444
dpetrovs <dspetrovskyy@gmail.com>
2016-11-29 02:05:49 +0200

    Homework 7_last

--- src/main/java/com/dpetrovs/app/runners/homework/lesson7/LoopsArraysRunner.java
+++ src/main/java/com/dpetrovs/app/runners/homework/lesson7/LoopsArraysRunner.java
@@ -1,27 +1,105 @@
 package com.dpetrovs.app.runners.homework.lesson7;
+
 import java.util.*;
 
 public class LoopsArraysRunner {
 
-    public int[] even(){
-        int[] evenMass = new int [10];
+    public int[] even0_20() {
+        int[] evenMass = new int[10];
         int k = 0;
-        for (int i=2;i<=20;i+=2){
+        for (int i = 2; i <= 20; i += 2) {
             evenMass[k] = i;
             k++;
         }
         return evenMass;
     }
 
-    protected void evenViewHoryzont(int[] a){
-        String intArrayString = Arrays.toString(a);
-            System.out.println(intArrayString);
+    public int[] odd1_99() {
+
+        int[] oddMass = new int[50];
+        int k = 0;
+        for (int i = 1; i < 100; i += 2) {
+            oddMass[k] = i;
+            k++;
+        }
+        return oddMass;
     }
 
-    protected void evenViewVert(int[] a){
-        for (int i=0; i<a.length;i++){
+    public int[] oddRevert1_99() {
+
+        int[] oddMass = new int[50];
+        int k = 0;
+        for (int i = 99; i > 0; i -= 2) {
+            oddMass[k] = i;
+            k++;
+        }
+        return oddMass;
+    }
+
+    public int[] arrRand0_15() {
+
+        int[] mass = new int[15];
+        for (int i = 0; i < mass.length; i++) {
+            mass[i] = (int) Math.round(Math.random() * 15);
+        }
+        return mass;
+    }
+
+    public int countRandElements(int[] a) {
+        int counter = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] % 2 == 0 && a[i] != 0) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public int[] randMass0_999() {
+
+        int[] mass = new int[15];
+        for (int i = 0; i < mass.length; i++) {
+            mass[i] = (int) Math.round(Math.random() * 999);
+        }
+        return mass;
+    }
+
+    public int maxElementInMassive(int[] a) {
+        int maxElement = 0;
+        int maxValue;
+        for (int i = 0; i < a.length; i++) {
+            if (a[maxElement] < a[i]) {
+                maxElement = i;
+            }
+        }
+        maxValue = a[maxElement];
+        return maxValue;
+    }
+    public int minElementInMassive(int[] a) {
+        int minElement = 0;
+        int minValue;
+        for (int i = 0; i < a.length; i++) {
+            if (a[minElement] > a[i]) {
+                minElement = i;
+            }
+        }
+        minValue = a[minElement];
+        return minValue;
+    }
+
+    protected void evenViewHoryzont(int[] a) {
+        String intArrayString = Arrays.toString(a);
+        System.out.println(intArrayString);
+    }
+
+    protected void evenViewVert(int[] a) {
+        for (int i = 0; i < a.length; i++) {
             System.out.println(a[i]);
         }
+    }
+
+    protected void counter(int a) {
+        System.out.println("counter value is " + a);
     }
 
 }
--- src/main/java/com/dpetrovs/app/runners/homework/lesson7/Main.java
+++ src/main/java/com/dpetrovs/app/runners/homework/lesson7/Main.java
@@ -4,9 +4,24 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        LoopsArraysRunner even0_20 = new LoopsArraysRunner();
-        even0_20.evenViewHoryzont(even0_20.even());
-        even0_20.evenViewVert(even0_20.even());
+        LoopsArraysRunner even = new LoopsArraysRunner();
+        even.evenViewHoryzont(even.even0_20());
+        even.evenViewVert(even.even0_20());
+
+        LoopsArraysRunner odd = new LoopsArraysRunner();
+        odd.evenViewHoryzont(odd.odd1_99());
+        odd.evenViewHoryzont(odd.oddRevert1_99());
+
+        LoopsArraysRunner mathRand0_15 = new LoopsArraysRunner();
+        int[] a = mathRand0_15.arrRand0_15();
+        mathRand0_15.evenViewHoryzont(a);
+        mathRand0_15.counter(mathRand0_15.countRandElements(a));
+
+        LoopsArraysRunner mathRand0_999 = new LoopsArraysRunner();
+        int[] b = mathRand0_999.randMass0_999();
+        mathRand0_999.evenViewHoryzont(b);
+        mathRand0_999.counter(mathRand0_999.maxElementInMassive(b));
+        mathRand0_999.counter(mathRand0_999.minElementInMassive(b));
     }
 
 }
