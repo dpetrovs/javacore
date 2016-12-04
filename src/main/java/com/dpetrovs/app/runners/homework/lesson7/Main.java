15347a90d024278eafc1a40d3e6c081fa1e8d154
dpetrovs <dspetrovskyy@gmail.com>
2016-12-04 16:30:56 +0200

    lesson 7

--- src/main/java/com/dpetrovs/app/runners/homework/lesson7/Main.java
+++ src/main/java/com/dpetrovs/app/runners/homework/lesson7/Main.java
@@ -4,24 +4,21 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        LoopsArraysRunner even = new LoopsArraysRunner();
-        even.evenViewHoryzont(even.even0_20());
-        even.evenViewVert(even.even0_20());
+        LoopsArraysRunner massiveLesson = new LoopsArraysRunner();
+        massiveLesson.evenViewHoryzont(massiveLesson.even0_20());
+        massiveLesson.evenViewVert(massiveLesson.even0_20());
 
-        LoopsArraysRunner odd = new LoopsArraysRunner();
-        odd.evenViewHoryzont(odd.odd1_99());
-        odd.evenViewHoryzont(odd.oddRevert1_99());
+        massiveLesson.evenViewHoryzont(massiveLesson.odd1_99());
+        massiveLesson.evenViewHoryzont(massiveLesson.oddRevert1_99());
 
-        LoopsArraysRunner mathRand0_15 = new LoopsArraysRunner();
-        int[] a = mathRand0_15.arrRand0_15();
-        mathRand0_15.evenViewHoryzont(a);
-        mathRand0_15.counter(mathRand0_15.countRandElements(a));
+        int[] a = massiveLesson.arrRand0_15();
+        massiveLesson.evenViewHoryzont(a);
+        massiveLesson.counter(massiveLesson.countRandElements(a));
 
-        LoopsArraysRunner mathRand0_999 = new LoopsArraysRunner();
-        int[] b = mathRand0_999.randMass0_999();
-        mathRand0_999.evenViewHoryzont(b);
-        mathRand0_999.counter(mathRand0_999.maxElementInMassive(b));
-        mathRand0_999.counter(mathRand0_999.minElementInMassive(b));
+        int[] b = massiveLesson.randMass0_999();
+        massiveLesson.evenViewHoryzont(b);
+        massiveLesson.counter(massiveLesson.maxElementInMassive(b));
+        massiveLesson.counter(massiveLesson.minElementInMassive(b));
     }
 
 }
---
+++ src/test/java/com/dpetrovs/app/Test.java
@@ -0,0 +1,7 @@
+package com.dpetrovs.app;
+
+/**
+ * Created by Dmitrik on 04.12.2016.
+ */
+public class Test {
+}
